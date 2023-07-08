93352e3d24b0b31fd1caa1c5825605072ed182c2
ppparkta <pppppark2@gmail.com>
2023-07-09 01:56:29 +0900

    feat: User Entity 설계

--- src/main/java/com/likelion/swu/User/UserEntity.java
+++ src/main/java/com/likelion/swu/User/UserEntity.java
@@ -1,4 +1,26 @@
 package com.likelion.swu.User;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+@ToString
 public class UserEntity {
+    @Id
+    @Column(name="user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(unique = true)
+    private long student_number;
+    private String name;
+    private String password;
+    private String major;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }

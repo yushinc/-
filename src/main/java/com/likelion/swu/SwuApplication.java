c75589e213b25d17918a8aa55065856d413ffbe3
ppparkta <pppppark2@gmail.com>
2023-07-09 00:34:20 +0900

    build: 새 프로젝트 생성

---
+++ src/main/java/com/likelion/swu/SwuApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.swu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/swu/SwuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.swu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
